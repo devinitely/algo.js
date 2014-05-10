deb0f7c0e6ac2597edc53367733481d8aeb91f8a
Timo B <devinitely@users.noreply.github.com>
2014-05-11 01:35:28 +0200

    quicksort

---
+++ quicksort.js
@@ -0,0 +1,91 @@
+var array = [1, 3, 2, 4, 6, 5, 8, 7, 9];
+
+/*
+var populateArray = function()
+{
+	$.get('IntegerArray.txt', function(data){
+	array = data.split('\n');
+	//console.log(array);
+	mergeSort(array);
+	});
+}
+*/
+
+var partition = function(arr, lftPtr, rthPtr)
+{
+	var pivot = arr[Math.floor((lftPtr+rthPtr)/2)];
+	var temp = "";
+	while(lftPtr <= rthPtr)	{
+		while(arr[lftPtr] < pivot)
+			lftPtr++;
+		while(arr[rthPtr] > pivot)
+			rthPtr--;
+		if(lftPtr <= rthPtr) {
+			temp = arr[lftPtr];
+			arr[lftPtr] = arr[rthPtr];
+			arr[rthPtr] = temp;
+			lftPtr++;
+			rthPtr--;
+		}
+	}
+	return lftPtr;		
+}
+
+var quickSort = function(arr, lftPtr, rthPtr)
+{
+	var idx;
+	if(arr.length > 1) {
+		idx = partition(arr, lftPtr, rthPtr);
+		if(lftPtr < idx-1) 
+			quickSort(arr, lftPtr, idx -1);
+		if(idx < rthPtr) {
+			quickSort(arr, idx, rthPtr);
+		}
+	}
+	return arr;
+}
+
+/*
+var mergeSort = function(arr)
+{
+	//console.log("arr.length =" + arr.length);
+	if(arr.length <= 1)
+		return arr;
+
+	var left = arr.slice(0, arr.length/2);
+	var right = arr.slice(arr.length/2, arr.length);
+	
+	//console.log("New Iteration: " + count++ + "!");
+	//console.log(left);
+	//console.log(right);
+
+	return merge(mergeSort(left), mergeSort(right));
+}
+
+var merge = function(left, right)
+{
+	var result = [];
+//	console.log("left.ln" + left.length);
+//	console.log("right.ln" + right.length);
+
+	while(left.length && right.length)
+	{
+//		console.log("left && right: " + count++);
+		if(left[0] <= right[0])
+		{
+			result.push(left.shift());
+		}
+		else
+		{
+			result.push(right.shift());
+		}
+	}
+	while(left.length)
+		result.push(left.shift());
+	while(right.length)
+		result.push(right.shift());
+
+	//console.log(result);
+	return result;
+}
+*/
